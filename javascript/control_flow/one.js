39a406a3abc41e48bf8a524c98eadd62625c5dab
Gajendra Singh <117335797+gajendrasingh806512@users.noreply.github.com>
2023-11-27 19:50:31 +0000

    change

---
+++ javascript/control_flow/one.js
@@ -0,0 +1,32 @@
+
+//if 
+const isUserloggedIn = true
+const temperature = 41
+
+
+// if (temperature === 41) {
+//     console.log("temprature is less then 50")
+    
+// }
+// else(
+//     console.log("Temperature is Greater then 50")
+// )
+
+
+
+const balance = 1000
+
+// if(balance>500) console.log("test");
+
+// if(balance >500){
+//     console.log("less than 500")
+// }else if(balance<750){
+//     console.log("less the 750")
+// }else if (balance<900){
+//     console.log("less the 900")
+// }else{
+//     console.log("less then 1200")
+// }
+
+
+if(isUserloggedIn)
