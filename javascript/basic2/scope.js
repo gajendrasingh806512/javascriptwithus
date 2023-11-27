61bdc65f921cd77968806bf356d1a0974a132767
Gajendra Singh <117335797+gajendrasingh806512@users.noreply.github.com>
2023-11-27 18:12:26 +0000

    change

--- javascript/basic2/scope.js
+++ javascript/basic2/scope.js
@@ -12,4 +12,42 @@ var c =30
 
 //console.log(a)
 //console.log(b)
-console.log(c)
+//console.log(c)
+
+function one(){
+
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username)
+
+    }
+    //console.log(website)
+    two()
+}
+
+//one()
+
+if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = "youtube"
+        //console.log(username+website)
+    }
+    //console.log(website)
+}
+
+//console.log(username)
+
+//++++++++++++intersting++++++++++
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+addtwo(5)
+const addtwo = function(num){
+    return num+2
+}
+
